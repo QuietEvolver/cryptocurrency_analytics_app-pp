50b0e0ee24b03aa039264a73c8fe5edd3fa1257d
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-16 10:54:31 -0800

    It will update the api request and response.
    Co-authored-by: Richard Cha <charichard09@gmail.com>
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/styles.css';
 
 // Business Logic
 
-function getWeather(city) {
+function getWeather() {//symbol
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  const url = `https://api.simpleswap.io/get_currency?api_key=15635da6-8163-42f5-8f20-77ba0f5b5fc6&symbol=btc`;// `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      printElements(response, city);
+      console.log("Response OK: ", response);
+      printElements(response);//, symbol
     } else {
-      printError(this, response, city);
+      console.log("Response err: ", response);
+      printError(this, response);//, symbol
     }
   });
 
@@ -23,20 +25,19 @@ function getWeather(city) {
 
 // UI Logic
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+function printElements(apiResponse) { //, symbol
+  document.querySelector('#showResponse').innerText = `${apiResponse.name}`; //}; ${symbol`;
 }
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printError(request, apiResponse) { //symbol 
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for : ${request.status} ${request.statusText}: ${apiResponse.message}`;//${symbol}
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const city = document.querySelector('#location').value;
+  //const symbol = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  getWeather(city);
+  getWeather();
 }
 
 window.addEventListener("load", function() {
